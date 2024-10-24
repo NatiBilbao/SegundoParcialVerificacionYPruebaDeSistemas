0214885e682927a311bbf1995ae786088c343fb6
NatyBilbao <natabilbaocano18@gmail.com>
2024-10-24 17:36:49 -0400

    Commit 1

---
+++ src/test/java/api/utils/Properties.java
@@ -0,0 +1,7 @@
+package api.utils;
+
+public class Properties {
+    public static String host = "https://todo.ly/";
+    public static String user = "nataliabilbaocano@gmail.com";
+    public static String pwd = "Admin123.";
+}
